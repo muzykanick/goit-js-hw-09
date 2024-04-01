4b3a5b68a8187babfd7afa22634d6bbeb4ba949a
muzykanick <muzykanick@yahoo.com>
2024-04-01 22:27:17 +0300

    commit final x1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,33 @@
-console.log("Form");
+const form = document.querySelector(".feedback-form");
+const input = form.elements.email;
+const textarea = form.elements.message;
+
+form.addEventListener("input", inputHandle);
+form.addEventListener("submit", submitHandle);
+
+const localStorageKey = "feedback-form-state";
+
+const storedData = JSON.parse(localStorage.getItem(localStorageKey));
+if (storedData) {
+  input.value = storedData.email ?? "";
+  textarea.value = storedData.message ?? "";
+}
+
+function inputHandle(event) {
+  let formData = {
+    email: event.currentTarget.elements.email.value.trim(),
+    message: event.currentTarget.elements.message.value.trim(),
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
+
+function submitHandle(event) {
+  event.preventDefault();
+  if (input.value !== "" && textarea.value !== "") {
+    console.log(JSON.parse(localStorage.getItem(localStorageKey)));
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+  } else {
+    alert("Please fill out the form");
+  }
+}
